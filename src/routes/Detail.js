711d4256fd191d917b387e140cfa45b5c6a8bb23
AndOne1shot <wnduddud97@naver.com>
2025-04-13 18:05:06 +0900

    연결

---
+++ src/routes/Detail.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  if (!movie) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <h1>Detail</h1>
+      <span>{movie.title}</span>
+    </div>
+  );
+}
+
+export default Detail;
