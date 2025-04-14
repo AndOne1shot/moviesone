d5444ba4bcbee98d3107c01e7bfbfb5e0ebd6535
AndOne1shot <wnduddud97@naver.com>
2025-04-14 12:06:38 +0900

    메인페이지 틀만 잡았음 아직 더해야됨

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,15 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "../mycss/mainpage.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
-    </div>
+    <Link to={`/movie/${id}`} className={styles.link}>
+      <div className={styles.container}>
+        <img src={coverImg} alt={title} className={styles.poster} />
+        <span>{title}</span>
+      </div>
+    </Link>
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "../mycss/mainpage.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -22,14 +23,13 @@ function Home() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className={styles.flex_container}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
               id={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
-              summary={movie.summary}
               genres={movie.genres}
             />
           ))}
